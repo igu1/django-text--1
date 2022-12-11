0dfd2d1aa3565ca7c40bc12c16bd6ed972a277a4
igu1 <eesaard@gmail.com>
2022-12-11 09:27:38 +0530

    #2

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import BlogForm
+from django.http import HttpResponseNotFound
 from .models import BlogModel, CommentModel
 from django.utils import timezone
 
@@ -12,6 +13,7 @@ def index(request, id):
     form = BlogForm()
     blog = {}
     message = {}
+    updateForm = {}
     try:
         blog = BlogModel.objects.get(id=id)
     except:
@@ -22,18 +24,20 @@ def index(request, id):
         }
         
     if request.method == 'POST':
+        updateForm = BlogForm(request.POST)
         if 'comment' not in request.POST:
-            form = BlogForm(request.POST, request.FILES)
+            form = BlogForm(request.POST)
             if form.is_valid():
                 data = form.save(commit=False)
                 data.user = request.user
                 data.created = timezone.now()
                 form.save()
-        else:
+        elif 'comment' in request.POST:
             message = request.POST['message']
             if message.strip() != "":
-                print(request.POST)
                 CommentModel.objects.create(message=message, user=request.user, blog=BlogModel.objects.get(id=request.POST['blog'])) 
+        # elif 'delete' in request.POST:
+        #     return redirect()
     comments = []
     try:
         comments = CommentModel.objects.filter(blog=BlogModel.objects.get(id=id))
@@ -41,6 +45,7 @@ def index(request, id):
         print('Error')
     return render(request, 'index.html', {
         'form': form,
+        'updateForm': updateForm, 
         'data': BlogModel.objects.all(),
         'blog': blog,
         'comments': comments
@@ -48,5 +53,8 @@ def index(request, id):
 
 
 def delete_blog(request, id):
-    BlogModel.objects.get(id=id).delete()
+    if request.user == BlogModel.objects.get(id=id).user:
+        BlogModel.objects.get(id=id).delete()
+    else:
+        return HttpResponseNotFound()
     return redirect('home')
